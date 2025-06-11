ea4fbe1f56fed158336860241ce6fde6135b0c50
wjames14 <hkrakm@sky.com>
2025-06-11 17:45:01 +0100

    Updating extract for specific route.

--- pipelines/live-data/extract.py
+++ pipelines/live-data/extract.py
@@ -5,19 +5,20 @@ import logging
 from dotenv import load_dotenv
 import requests
 import pandas as pd
+from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def fetch_station_json(crs: str) -> dict:
+def fetch_station_json(start_crs: str, end_crs: str) -> dict:
     """Fetches JSON data from the Realtime Trains API for a given CRS code."""
-    if not isinstance(crs, str):
+    if not isinstance(start_crs, str) and isinstance(end_crs, str):
         logger.error("Invalid CRS: %s. Expected a string.", crs)
         raise ValueError("The CRS must be a string.")
-    url = f"https://api.rtt.io/api/v1/json/search/{crs}"
+    url = f"https://api.rtt.io/api/v1/json/search/{start_crs}/to/{end_crs}"
     try:
         response = requests.get(url=url, auth=(ENV["API_USERNAME"], ENV["API_PASSWORD"]), timeout=5)
-        logger.info("Successfully fetched data for CRS: %s", crs)
+        logger.info("Successfully connected to '%s'", url)
         return response.json()
     except requests.exceptions.RequestException as e:
         logger.error("Request failed for CRS %s: %s", crs, e)
@@ -27,7 +28,7 @@ def get_station_name(response: dict):
     """Returns the station name from the api response."""
     location = response.get('location')
     if location:
-        name = location.get('name', 'Unknown')
+        name = location.get('name')
         logger.info("Extracted station name: %s", name)
         return name
     logger.warning("No location data found in response.")
@@ -36,32 +37,55 @@ def get_station_name(response: dict):
 def get_trains(response: dict) -> list[dict]:
     """Extracts the list of train services from the API response."""
     services = response.get('services', [])
+    if services == []:
+        logger.debug("No services to retrieve.")
+        return services
     logger.info("Train services successfully retrieved from API response.")
     return services
 
 def extract_train_info(service: dict, name: str, crs: str) -> dict:
     """ Extracts key train information fields from a single service dictionary."""
-    logger.debug("Extracting train info for services at %s", crs)
+    logger.debug("Extracting train info for services from %s", crs)
     location_detail = service.get("locationDetail", {})
     origin = location_detail.get("origin", [{}])[0]
     destination = location_detail.get("destination", [{}])[0]
 
-    service_info =  {'serviceUid': service.get('serviceUid'),
-            'station_name': name,
-            'station_crs': crs,
-            'origin_name': origin.get("description", ""),
-            'origin_tiploc': origin.get("tiploc", ""),
-            'destination_name': destination.get("description", ""),
-            'destination_tiploc': destination.get("tiploc", ""),
-            'realtimeActivated': location_detail.get("realtimeActivated", False),
-            'gbttBookedDeparture': location_detail.get("gbttBookedDeparture", ""),
-            'isCall': location_detail.get("isCall", False),
-            'realtimeDeparture': location_detail.get("realtimeDeparture", ""),
-            'atocName': service.get("atocName", ""),
-            'runDate': service.get("runDate", ""),
-            'displayAs': location_detail.get("displayAs", "")
-            }
-    logger.info("Extracted train info for service '%s'", service_info.get('serviceUid'))
+    # service_info =  {'serviceUid': service.get('serviceUid'),
+    #                 'train_identity': service.get("trainIdentity"),
+    #                 'station_name': name,
+    #                 'station_crs': crs,
+    #                 'origin_name': origin.get("description"),
+    #                 'destination_name': destination.get("description"),
+    #                 'gbttBookedArrival': location_detail.get("gbttBookedArrival"),
+    #                 'realtimeArrival': location_detail.get("realtimeArrival"),
+    #                 'gbttBookedDeparture': location_detail.get("gbttBookedDeparture"),
+    #                 'realtimeDeparture': location_detail.get("realtimeDeparture"),
+    #                 'atocName': service.get("atocName"),
+    #                 'runDate': service.get("runDate"),
+    #                 'platform': location_detail.get("platform"),
+    #                 'platform_changed': location_detail.get("platformChanged"),
+    #                 'cancelled': bool(location_detail.get('cancelReasonCode')),
+    #                 'cancel_reason': location_detail.get('cancelReasonLongText')
+    #                 }
+    service_info =  {'service_uid': service.get('serviceUid'),
+                    'train_identity': service.get("trainIdentity"),
+                    'station_name': name,
+                    'station_crs': crs,
+                    'origin_name': origin.get("description"),
+                    'destination_name': destination.get("description"),
+                    'scheduled_arr_time': location_detail.get("gbttBookedArrival"),
+                    'actual_arr_time': location_detail.get("realtimeArrival"),
+                    'scheduled_dep_time': location_detail.get("gbttBookedDeparture"),
+                    'actual_dep_time': location_detail.get("realtimeDeparture"),
+                    'operator_name': service.get("atocName"),
+                    'service_date': service.get("runDate"),
+                    'platform': location_detail.get("platform"),
+                    'platform_changed': location_detail.get("platformChanged"),
+                    'cancelled': bool(location_detail.get('cancelReasonCode')),
+                    'cancel_reason': location_detail.get('cancelReasonLongText')
+                    }
+
+    logger.info("Extracted train info for service '%s'", service_info.get('service_uid'))
     return service_info
 
 def make_train_info_list(train_list: list[dict], name:str, crs: str) -> list[dict]:
@@ -75,28 +99,33 @@ def make_dataframe(train_data: list[dict]) -> pd.DataFrame:
     logger.info("Created dataframe with %d records.", len(df))
     return df
 
-def get_service_dataframe(crs:str) -> pd.DataFrame:
+def get_service_dataframe(start_crs:str, end_crs: str) -> pd.DataFrame:
     """Retrieves, processes, and returns train data as a dataframe for a given station CRS."""
-    logger.debug("Retrieving service dataframe for CRS: %s", crs)
-    data = fetch_station_json(crs)
+    logger.debug("Retrieving service dataframe for service from %s - %s", start_crs, end_crs)
+    data = fetch_station_json(start_crs, end_crs)
     trains = get_trains(data)
     name = get_station_name(data)
-    trains_list = make_train_info_list(trains, name, crs)
+    trains_list = make_train_info_list(trains, name, start_crs)
     df = make_dataframe(trains_list)
-    logger.info("Retrieved service dataframe for CRS: %s", crs)
+    logger.info("Retrieved service dataframe for CRS: %s", start_crs)
     return df
 
-def fetch_train_data(station_list: list[str]) -> pd.DataFrame:
+def fetch_train_data(station_list: list[list]) -> pd.DataFrame:
     """Returns a dataframe of the services given a list of station crs."""
     logger.debug("Fetching service data for stations: %s", station_list)
     station_dfs = []
-    for crs in station_list:
-        station_dfs.append(get_service_dataframe(crs))
+    for a,b in station_list:
+        station_dfs.append(get_service_dataframe(a,b))
     aggregated_df = pd.concat(station_dfs, ignore_index=True)
     logger.info("Fetched service data for %d stations.", len(station_list))
     return aggregated_df
 
 if __name__ == "__main__":
     load_dotenv()
-    df = fetch_train_data(['LTV'])
-    print(df)
+    result = fetch_train_data([['PAD', 'BRI'], 
+                               ['RDG', 'BRI'], 
+                               ['DID', 'BRI'], 
+                               ['SWI', 'BRI'],
+                               ['CPM', 'BRI'],
+                               ['BTH', 'BRI']])
+    print(result.head())
---
+++ pipelines/live-data/test_extract.py
@@ -0,0 +1,169 @@
+"""Unit testing for the functions in extract.py."""
+from unittest.mock import patch
+
+from extract import get_station_name, get_trains, extract_train_info, make_train_info_list
+
+### Testing get_station_name() ###
+def test_get_station_name_returns_name_when_location_is_present():
+    response = {"location": {"name": "test"}}
+    assert get_station_name(response) == "test"
+
+def test_get_station_name_returns_none_when_name_is_none():
+    response = {"location": {"name": None}}
+    assert get_station_name(response) is None
+
+def test_get_station_name_returns_none_when_location_is_none():
+    response = {"location": None}
+    assert get_station_name(response) is None
+
+@patch('extract.logger')
+def test_logs_info_when_location_is_present(mock_logger):
+    response = {'location': {'name': 'test'}}
+    get_station_name(response)
+    mock_logger.info.assert_called_once_with("Extracted station name: %s", 'test')
+
+@patch('extract.logger')
+def test_logs_warning_when_location_is_missing(mock_logger):
+    response = {}
+    get_station_name(response)
+    mock_logger.warning.assert_called_once_with("No location data found in response.")
+
+### Testing get_trains() ###
+def test_get_trains_returns_empty_list_when_no_services():
+    response = {}
+    assert get_trains(response) == []
+
+def test_get_trains_returns_services_when_present():
+    response = {'services': [{'service': 1}, {'service': 2}]}
+    assert get_trains(response) == [{'service': 1}, {'service': 2}]
+
+@patch('extract.logger')
+def test_logs_info_when_services_are_retrieved(mock_logger):
+    response = {'services': [{'service': 1}]}
+    get_trains(response)
+    mock_logger.info.assert_called_once_with("Train services successfully retrieved from API response.")
+
+@patch('extract.logger')
+def test_logs_info_when_no_services_in_response(mock_logger):
+    response = {}
+    get_trains(response)
+    mock_logger.debug.assert_called_once_with("No services to retrieve.")
+
+### Testing extract_train_info() ###
+def test_extract_train_info_with_valid_service():
+    service = {
+    'serviceUid': '123',
+    'trainIdentity': '234',
+    'atocName': 'test',
+    'runDate': '0000-00-00',
+    'locationDetail': {
+        'origin': [{'description': 'arr_description'}],
+        'destination': [{'description': 'des_description'}],
+        'gbttBookedArrival': '1000',
+        'realtimeArrival': '2000',
+        'gbttBookedDeparture': '3000',
+        'realtimeDeparture': '4000',
+        'platform': '1',
+        'platformChanged': False
+    }
+}
+
+    name = 'Station Name'
+    crs = 'ABC'
+    expected = {
+        'serviceUid': '123',
+        'train_identity': '234',
+        'station_name': 'Station Name',
+        'station_crs': 'ABC',
+        'origin_name': 'arr_description',
+        'destination_name': 'des_description',
+        'gbttBookedArrival': '1000',
+        'realtimeArrival': '2000',
+        'gbttBookedDeparture': '3000',
+        'realtimeDeparture': '4000',
+        'atocName': 'test',
+        'runDate': '0000-00-00',
+        'platform': '1',
+        'platform_changed': False,
+        'cancelled': False,
+        'cancel_reason': None
+    }
+    result = extract_train_info(service, name, crs)
+    assert result == expected
+
+def test_extract_train_info_with_cancelled_service():
+    service = {
+    'serviceUid': '123',
+    'trainIdentity': '234',
+    'atocName': 'test',
+    'runDate': '0000-00-00',
+    'locationDetail': {
+        'origin': [{'description': 'arr_description'}],
+        'destination': [{'description': 'des_description'}],
+        'gbttBookedArrival': '1000',
+        'realtimeArrival': '2000',
+        'gbttBookedDeparture': '3000',
+        'realtimeDeparture': '4000',
+        'platform': '1',
+        'platformChanged': False,
+        'cancelReasonCode': 'abc',
+        'cancelReasonLongText': 'cancelled'
+    }
+}
+
+    name = 'Station Name'
+    crs = 'ABC'
+    expected = {
+        'serviceUid': '123',
+        'train_identity': '234',
+        'station_name': 'Station Name',
+        'station_crs': 'ABC',
+        'origin_name': 'arr_description',
+        'destination_name': 'des_description',
+        'gbttBookedArrival': '1000',
+        'realtimeArrival': '2000',
+        'gbttBookedDeparture': '3000',
+        'realtimeDeparture': '4000',
+        'atocName': 'test',
+        'runDate': '0000-00-00',
+        'platform': '1',
+        'platform_changed': False,
+        'cancelled': True,
+        'cancel_reason': 'cancelled'
+    }
+    result = extract_train_info(service, name, crs)
+    assert result == expected
+
+
+@patch('extract.logger')
+def test_logs_debug_when_extracting_train_info(mock_logger):
+    service = {'serviceUid': '123'}
+    name = 'station1'
+    crs = 'ABC'
+    extract_train_info(service, name, crs)
+    mock_logger.debug.assert_called_once_with("Extracting train info for services at %s", "ABC")
+
+@patch('extract.logger')
+def test_logs_info_when_train_info_extracted(mock_logger):
+    service = {'serviceUid': '123'}
+    name = 'station1'
+    crs = 'ABC'
+    extract_train_info(service, name, crs)
+    mock_logger.info.assert_called_once_with("Extracted train info for service '%s'", '123')
+
+### Testing make_train_info_list() ###
+
+def test_make_train_info_list():
+    train_list = [{'serviceUid': '123'}, {'serviceUid': '456'}]
+    name = 'station1'
+    crs = 'st1'
+
+    with patch('extract.extract_train_info') as mock_extract_train_info:
+        mock_extract_train_info.side_effect = [{'serviceUid': '123', 'station_name': 'Station A'},
+                                               {'serviceUid': '456', 'station_name': 'Station A'}
+                                               ]
+        result = make_train_info_list(train_list, name, crs)
+
+        assert len(result) == 2
+        assert result[0]['serviceUid'] == '123'
+        assert result[1]['serviceUid'] == '456'
