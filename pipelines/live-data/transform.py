edb90a9a314a98c66b55b5e7344e6fc4c3862526
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-12 14:02:47 +0100

    Add extract script data.

--- pipelines/live-data/transform.py
+++ pipelines/live-data/transform.py
@@ -1,10 +1,12 @@
 """Script to clean the raw train data."""
+from os import environ as ENV
 import logging
 
+from dotenv import load_dotenv
 import pandas as pd
 from pandas import DataFrame
 
-logger = logging.getLogger(__name__)
+from extract import fetch_train_data
 
 logging.basicConfig(
     level="WARNING",
@@ -12,12 +14,22 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%H:%M:%S"
 )
 
+logger = logging.getLogger(__name__)
+
 
 REQUIRED_COLUMNS = [
     "service_uid", "train_identity", "station_name", "station_crs",
     "origin_name", "destination_name", "scheduled_arr_time", "actual_arr_time",
     "scheduled_dep_time", "actual_dep_time", "operator_name",
     "service_date", "platform", "platform_changed", "cancelled", "cancel_reason"
+]
+
+ORIGIN_STATION = [
+    ""
+]
+
+DESTINATION_STATION = [
+    "Bristol Temple Mead"
 ]
 
 
@@ -34,4 +46,7 @@ def check_all_required_columns_present(data: DataFrame, required_columns=REQUIRE
 
 
 if __name__ == "__main__":
-    pass
+    load_dotenv()
+    stations = ['PAD', 'RDG', 'DID', 'SWI', 'CPM', 'BTH', 'BRI']
+    result = fetch_train_data(stations)
+    check_all_required_columns_present(result)
