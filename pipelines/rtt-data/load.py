2990b71fdb5dce357c77d55a5223f51fa9ec8d70
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-13 14:46:32 +0100

    Add logging to get_connection().

--- pipelines/rtt-data/load.py
+++ pipelines/rtt-data/load.py
@@ -1,31 +1,44 @@
 """Script for loading data into RDS."""
 
-from logging import getLogger
+import logging
 from os import environ as ENV
 
 import pandas as pd
 from pandas import DataFrame
 
 from dotenv import load_dotenv
-from psycopg2 import connect
+from psycopg2 import connect, OperationalError
 from psycopg2.extras import RealDictCursor
+
+logging.basicConfig(
+    level="DEBUG",
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S"
+)
+
+logger = logging.getLogger(__name__)
 
 
 def get_connection():
     """Return a database connection."""
-    conn = connect(
-        dbname=ENV["DB_NAME"],
-        user=ENV["DB_USER"],
-        password=ENV["DB_PASSWORD"],
-        host=ENV["DB_HOST"],
-        port=ENV["DB_PORT"],
-        cursor_factory=RealDictCursor
-    )
-
-    return conn
+    logger.info("Establishing connection to database...")
+    try:
+        conn = connect(
+            dbname=ENV["DB_NAME"],
+            user=ENV["DB_USER"],
+            password=ENV["DB_PASSWORD"],
+            host=ENV["DB_HOST"],
+            port=ENV["DB_PORT"],
+            cursor_factory=RealDictCursor
+        )
+        logger.info("Successfully connected to database.")
+        return conn
+    except OperationalError as e:
+        logger.error("Failed to connect to database: %s", e)
+        raise
 
 
 if __name__ == "__main__":
     load_dotenv()
     with get_connection() as db_connection:
-        print("Connected to database.")
+        logger.info("Connection established.")
