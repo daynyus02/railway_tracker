ddb19c84acdad4f884534ee823104b2b692b9767
Stefanrmc <stefanrcole@gmail.com>
2025-06-15 16:36:38 +0100

    Add time conversion function and first three operation functions.

--- report/conftest.py
+++ report/conftest.py
@@ -1,9 +1,10 @@
 """Fixtures for testing report files."""
 
-import datetime
+import datetime as dt
 
 from pytest import fixture
 from psycopg2.extras import RealDictRow
+from pandas import DataFrame
 
 
 @fixture
@@ -14,17 +15,17 @@ def test_valid_past_day_data():
             'train_stop_id': 108,
             'train_service_id': 65,
             'station_id': 11,
-            'scheduled_arr_time': datetime.time(21, 20),
-            'actual_arr_time': datetime.time(21, 18),
-            'scheduled_dep_time': datetime.time(21, 21),
-            'actual_dep_time': datetime.time(21, 21),
+            'scheduled_arr_time': dt.time(21, 20),
+            'actual_arr_time': dt.time(21, 18),
+            'scheduled_dep_time': dt.time(21, 21),
+            'actual_dep_time': dt.time(21, 21),
             'platform': '2',
             'platform_changed': False,
             'station_name': 'Didcot Parkway',
             'station_crs': 'DID',
             'service_uid': 'G04992',
             'train_identity': '1L90',
-            'service_date': datetime.date(2025, 6, 14),
+            'service_date': dt.date(2025, 6, 14),
             'route_id': 9,
             'cancellation_id': 13,
             'reason': 'a problem with the train'
@@ -33,17 +34,17 @@ def test_valid_past_day_data():
             'train_stop_id': 119,
             'train_service_id': 73,
             'station_id': 11,
-            'scheduled_arr_time': datetime.time(22, 20),
-            'actual_arr_time': datetime.time(22, 18),
-            'scheduled_dep_time': datetime.time(23, 45),
-            'actual_dep_time': datetime.time(23, 45),
+            'scheduled_arr_time': dt.time(22, 20),
+            'actual_arr_time': dt.time(22, 18),
+            'scheduled_dep_time': dt.time(23, 45),
+            'actual_dep_time': dt.time(23, 45),
             'platform': '5',
             'platform_changed': False,
             'station_name': 'Didcot Parkway',
             'station_crs': 'DID',
             'service_uid': 'G05996',
             'train_identity': '2P86',
-            'service_date': datetime.date(2025, 6, 14),
+            'service_date': dt.date(2025, 6, 14),
             'route_id': 6,
             'cancellation_id': None,
             'reason': None
@@ -52,16 +53,16 @@ def test_valid_past_day_data():
             'train_stop_id': 20,
             'train_service_id': 27,
             'station_id': 11,
-            'scheduled_arr_time': datetime.time(16, 26),
-            'actual_arr_time': datetime.time(16, 35),
-            'scheduled_dep_time': datetime.time(17, 8),
-            'actual_dep_time': datetime.time(17, 15),
+            'scheduled_arr_time': dt.time(16, 26),
+            'actual_arr_time': dt.time(16, 35),
+            'scheduled_dep_time': dt.time(17, 8),
+            'actual_dep_time': dt.time(17, 15),
             'platform': '5', 'platform_changed': False,
             'station_name': 'Didcot Parkway',
             'station_crs': 'DID',
             'service_uid': 'G05985',
             'train_identity': '2P61',
-            'service_date': datetime.date(2025, 6, 14),
+            'service_date': dt.date(2025, 6, 14),
             'route_id': 6,
             'cancellation_id': None,
             'reason': None
@@ -70,17 +71,17 @@ def test_valid_past_day_data():
             'train_stop_id': 25,
             'train_service_id': 29,
             'station_id': 11,
-            'scheduled_arr_time': datetime.time(17, 20),
-            'actual_arr_time': datetime.time(17, 25),
-            'scheduled_dep_time': datetime.time(18, 38),
-            'actual_dep_time': datetime.time(18, 38),
+            'scheduled_arr_time': dt.time(17, 20),
+            'actual_arr_time': dt.time(17, 25),
+            'scheduled_dep_time': dt.time(18, 38),
+            'actual_dep_time': dt.time(18, 38),
             'platform': '5',
             'platform_changed': False,
             'station_name': 'Didcot Parkway',
             'station_crs': 'DID',
             'service_uid': 'G05988',
             'train_identity': '2P67',
-            'service_date': datetime.date(2025, 6, 14),
+            'service_date': dt.date(2025, 6, 14),
             'route_id': 6,
             'cancellation_id': None,
             'reason': None
@@ -91,90 +92,98 @@ def test_valid_past_day_data():
 @fixture
 def past_day_data_no_delays():
     """Sample train database data with no delays."""
-    return [
+    return DataFrame([
         {
-            'scheduled_arr_time': datetime.time(21, 20),
-            'actual_arr_time': datetime.time(21, 20),
-            'scheduled_dep_time': datetime.time(21, 21),
-            'actual_dep_time': datetime.time(21, 21),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(21, 20),
+            'actual_arr_time': dt.time(21, 20),
+            'scheduled_dep_time': dt.time(21, 21),
+            'actual_dep_time': dt.time(21, 21),
             'cancellation_id': None,
             'reason': None
         },
         {
-            'scheduled_arr_time': datetime.time(22, 20),
-            'actual_arr_time': datetime.time(22, 20),
-            'scheduled_dep_time': datetime.time(23, 45),
-            'actual_dep_time': datetime.time(23, 45),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(22, 20),
+            'actual_arr_time': dt.time(22, 20),
+            'scheduled_dep_time': dt.time(23, 45),
+            'actual_dep_time': dt.time(23, 45),
             'cancellation_id': None,
             'reason': None
         }
-    ]
+    ])
 
 
 @fixture
 def past_day_data_long_delays():
     """Sample train database data with a train delayed by 5+ minutes (arrival & departure)."""
-    return [
+    return DataFrame([
         {
-            'scheduled_arr_time': datetime.time(21, 20),
-            'actual_arr_time': datetime.time(21, 29),
-            'scheduled_dep_time': datetime.time(21, 21),
-            'actual_dep_time': datetime.time(21, 27),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(21, 20),
+            'actual_arr_time': dt.time(21, 29),
+            'scheduled_dep_time': dt.time(21, 21),
+            'actual_dep_time': dt.time(21, 27),
             'cancellation_id': None,
             'reason': None
         },
         {
-            'scheduled_arr_time': datetime.time(22, 20),
-            'actual_arr_time': datetime.time(22, 20),
-            'scheduled_dep_time': datetime.time(23, 45),
-            'actual_dep_time': datetime.time(23, 45),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(22, 20),
+            'actual_arr_time': dt.time(22, 20),
+            'scheduled_dep_time': dt.time(23, 45),
+            'actual_dep_time': dt.time(23, 45),
             'cancellation_id': None,
             'reason': None
         }
-    ]
+    ])
 
 
 @fixture
 def past_day_data_cancellation():
     """Sample train database data with a train cancelled."""
-    return [
+    return DataFrame([
         {
-            'scheduled_arr_time': datetime.time(21, 20),
-            'actual_arr_time': datetime.time(21, 29),
-            'scheduled_dep_time': datetime.time(21, 21),
-            'actual_dep_time': datetime.time(21, 27),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(21, 20),
+            'actual_arr_time': dt.time(21, 29),
+            'scheduled_dep_time': dt.time(21, 21),
+            'actual_dep_time': dt.time(21, 27),
             'cancellation_id': None,
             'reason': None
         },
         {
-            'scheduled_arr_time': datetime.time(22, 20),
-            'actual_arr_time': datetime.time(22, 20),
-            'scheduled_dep_time': datetime.time(23, 45),
-            'actual_dep_time': datetime.time(23, 45),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(22, 20),
+            'actual_arr_time': dt.time(22, 20),
+            'scheduled_dep_time': dt.time(23, 45),
+            'actual_dep_time': dt.time(23, 45),
             'cancellation_id': 13,
             'reason': 'a problem with the train'
         }
-    ]
+    ])
 
 
 @fixture
 def past_day_data_early():
     """Sample train database data with a train that arrives and departs early."""
-    return [
+    return DataFrame([
         {
-            'scheduled_arr_time': datetime.time(21, 20),
-            'actual_arr_time': datetime.time(21, 18),
-            'scheduled_dep_time': datetime.time(21, 21),
-            'actual_dep_time': datetime.time(21, 20),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(21, 20),
+            'actual_arr_time': dt.time(21, 18),
+            'scheduled_dep_time': dt.time(21, 21),
+            'actual_dep_time': dt.time(21, 20),
             'cancellation_id': None,
             'reason': None
         },
         {
-            'scheduled_arr_time': datetime.time(22, 20),
-            'actual_arr_time': datetime.time(22, 22),
-            'scheduled_dep_time': datetime.time(23, 45),
-            'actual_dep_time': datetime.time(23, 48),
+            'service_date': dt.date(2025, 6, 14),
+            'scheduled_arr_time': dt.time(22, 20),
+            'actual_arr_time': dt.time(22, 22),
+            'scheduled_dep_time': dt.time(23, 45),
+            'actual_dep_time': dt.time(23, 48),
             'cancellation_id': None,
             'reason': None
         }
-    ]
+    ])
--- report/transform_pdf_summary.py
+++ report/transform_pdf_summary.py
@@ -1,43 +1,71 @@
 """Script for transforming extracted data from RDS into PDF summary report."""
 
-import datetime
+import datetime as dt
 
 import pandas as pd
 from pandas import DataFrame
 
 
-def get_pct_trains_dep_delayed_five_mins(station_data: list[dict]) -> float:
+def convert_train_times_to_date_times(data: DataFrame) -> DataFrame:
+    """Convert time columns to datetime objects to allow maths operations."""
+
+    train_date = data.loc[0, "service_date"]
+
+    time_columns = ["scheduled_arr_time", "actual_arr_time",
+                    "scheduled_dep_time", "actual_dep_time"]
+
+    for column in time_columns:
+        data[column] = data[column].apply(
+            lambda t: dt.datetime.combine(train_date, t))
+
+    return data
+
+
+def get_pct_trains_dep_delayed_five_mins(data: DataFrame) -> float:
     """Gets the percentage of trains with departure delayed by five or more minutes."""
 
-    station_data = DataFrame(station_data)
+    data = convert_train_times_to_date_times(data)
+
+    delayed_trains = len(data[data["actual_dep_time"] >
+                         data["scheduled_dep_time"]+dt.timedelta(minutes=5)])
+
+    return delayed_trains/len(data) * 100
 
 
-def get_pct_trains_arr_delayed_five_mins(station_data: list[dict]) -> float:
+def get_pct_trains_arr_delayed_five_mins(data: DataFrame) -> float:
     """Gets the percentage of trains with departure delayed by five or more minutes."""
-    ...
+
+    data = convert_train_times_to_date_times(data)
+
+    delayed_trains = len(data[data["actual_arr_time"] >
+                              data["scheduled_arr_time"]+dt.timedelta(minutes=5)])
+
+    return delayed_trains/len(data) * 100
 
 
-def get_pct_trains_cancelled(station_data: list[dict]) -> float:
+def get_pct_trains_cancelled(data: DataFrame) -> float:
     """Gets the percentage of trains cancelled."""
-    ...
+
+    cancelled_trains = int(data["cancellation_id"].count())
+
+    return cancelled_trains/len(data) * 100
 
 
-def get_avg_dep_delay_all_trains(station_data: list[dict]) -> float:
+def get_avg_dep_delay_all_trains(data: DataFrame) -> float:
     """Gets the average departure delay of all trains."""
-    ...
 
 
-def get_avg_arr_delay_all_trains(station_data: list[dict]) -> float:
+def get_avg_arr_delay_all_trains(data: DataFrame) -> float:
     """Gets the average arrival delay of all trains."""
     ...
 
 
-def get_avg_dep_delay_delayed_trains(station_data: list[dict]) -> float:
+def get_avg_dep_delay_delayed_trains(data: DataFrame) -> float:
     """Gets the average departure delay of trains delayed at least one minute."""
     ...
 
 
-def get_avg_arr_delay_delayed_trains(station_data: list[dict]) -> float:
+def get_avg_arr_delay_delayed_trains(data: DataFrame) -> float:
     """Gets the average arrival delay of trains delayed at least one minute."""
     ...
 
