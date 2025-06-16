68e41ddeacc94c03bbd745be5b74c70f366f74cd
tomkollig <195304969+tomkollig@users.noreply.github.com>
2025-06-16 11:17:56 +0100

    add transform

---
+++ pipelines/incidents-data/transform_incidents.py
@@ -0,0 +1,39 @@
+"""A script to transform data from the National Rail Incidents API."""
+
+from os import environ as ENV
+import logging
+
+from dotenv import load_dotenv
+import pandas as pd
+
+from extract_incidents import extract
+
+logging.basicConfig(
+    level="DEBUG",
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S"
+)
+
+logger = logging.getLogger(__name__)
+
+ORIGIN = "London Paddington"
+DEST = "Bristol Temple Meads"
+
+
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """Transform the data to be correct data types."""
+    logging.debug("Transform started.")
+    df["start_time"] = pd.to_datetime(df["start_time"], utc=True)
+    df["end_time"] = pd.to_datetime(df["end_time"], utc=True)
+    df["is_planned"] = df["is_planned"].map(
+        {"true": True, "false": False}).fillna(False)
+
+    logging.debug(df.dtypes)
+
+    return df
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    data = extract()
+    transform(data)
