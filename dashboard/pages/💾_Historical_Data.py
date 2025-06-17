d2fc685b35799ef2b13bd0b6a0549237a1528f8f
wjames14 <hkrakm@sky.com>
2025-06-17 17:23:02 +0100

    Adding subscription options.

--- dashboard/pages/💾_Historical_Data.py
+++ dashboard/pages/💾_Historical_Data.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import streamlit as st
 import psycopg2
 import pandas as pd
-
+import boto3
 
 from utils.live_data_dataframes import convert_times_to_datetime, add_status_column, add_delay_time, get_delays
 from utils.historical_data_visualisations import make_delays_heatmap
@@ -18,30 +18,55 @@ def fetch_data(_connection)-> pd.DataFrame:
     _connection.close()
     return data
 
-load_dotenv()
-conn = psycopg2.connect(host=ENV['DB_HOST'],
-                            port=ENV['DB_PORT'],
-                            dbname=ENV['DB_NAME'],
-                            user=ENV['DB_USER'],
-                            password=ENV['DB_PASSWORD'])
-data = fetch_data(conn)
-convert_times_to_datetime(data)
-add_status_column(data)
+def station_to_crs(station_name: str) -> str:
+    crs_codes = {
+    "London Paddington": "PAD",
+    "Bristol Temple Meads": "BRI",
+    "Reading": "RDG",
+    "Didcot Parkway": "DID",
+    "Bath Spa": "BTH"
+}
+    return crs_codes.get(station_name)
 
-delays=get_delays(data)
-delays = add_delay_time(delays)
-heatmap = make_delays_heatmap(delays)
-st.altair_chart(heatmap)
+if __name__ == '__main__':
+    load_dotenv()
+    conn = psycopg2.connect(host=ENV['DB_HOST'],
+                                port=ENV['DB_PORT'],
+                                dbname=ENV['DB_NAME'],
+                                user=ENV['DB_USER'],
+                                password=ENV['DB_PASSWORD'])
+    data = fetch_data(conn)
+    convert_times_to_datetime(data)
+    add_status_column(data)
 
-st.subheader("Subscriptions: ")
-with st.form("subscriptions"):
-    st.write("Please select a route: ")
-    unique_routes_list = get_unique_routes(data)
-    st.selectbox("Route", ["All"] + unique_routes_list)
-    delay_information = st.checkbox("Delay Information")
-    distruption = st.checkbox("Disruption")
-    email = st.text_input("Email address")
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        st.write("Thanks for submitting!")
+    delays=get_delays(data)
+    delays = add_delay_time(delays)
+    heatmap = make_delays_heatmap(delays)
+    st.altair_chart(heatmap)
 
+    st.subheader("Subscriptions: ")
+    with st.form("subscriptions"):
+        st.write("Please select a route: ")
+        unique_routes_list = get_unique_routes(data)
+        route = st.selectbox("Route", ["All"] + unique_routes_list)
+        delay_information = st.checkbox("Delay Information")
+        reports = st.checkbox("Reports")
+        email = st.text_input("Email address")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write("Thanks for submitting!")
+            stations = route.split(" to ")
+            station1 = station_to_crs(stations[0])
+            station2 = station_to_crs(stations[1])
+            sns_client = boto3.client('sns')
+            if reports:
+                topic_name = ENV["TOPIC_PREFIX"] + "delays" + f"{station1}-{station2}"
+                sns_client.create_topic(
+                    Name= topic_name
+                )
+            if delay_information:
+                topic_name = ENV["TOPIC_PREFIX"] + "reports" + f"{station1}-{station2}"
+                sns_client.create_topic(
+                    Name= topic_name
+                )
+            st.markdown(f"{topic_name}")
