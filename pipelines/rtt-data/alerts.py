579c7172b6e09811065e11d66ee7e1f04491fd3e
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-17 17:34:28 +0100

    Add send_notification()

--- pipelines/rtt-data/alerts.py
+++ pipelines/rtt-data/alerts.py
@@ -1,5 +1,7 @@
 """Modules/Functions required to push information to station topics."""
 from pandas import DataFrame
+from datetime import timedelta
+import boto3
 
 
 def get_delayed_trains(api_train_data: DataFrame) -> DataFrame:
@@ -13,11 +15,25 @@ def get_delayed_trains(api_train_data: DataFrame) -> DataFrame:
         api_train_data["scheduled_dep_time"]
     )
 
-    delayed = api_train_data[delayed_trains_mask]
-
-    print(delayed[["scheduled_dep_time", "actual_dep_time"]])
-
     return api_train_data[delayed_trains_mask]
+
+
+def filter_by_route(api_train_data: DataFrame, origin: str, destination: str) -> DataFrame:
+    """Filters delayed trains for a specific route."""
+    return api_train_data[
+        api_train_data["origin_name"] == origin,
+        api_train_data["destination_name"] == destination
+    ]
+
+
+def send_notification(api_train_data: DataFrame):
+    sns_client = boto3.client("sns")
+    delayed_trains = get_delayed_trains(api_train_data)
+
+    paddington_to_bristol_route = filter_by_route(
+        delayed_trains, "London Paddington", "Bristol Temple Meads")
+
+    print(paddington_to_bristol_route)
 
 
 if __name__ == "__main__":
