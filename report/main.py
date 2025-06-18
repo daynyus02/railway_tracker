2abb610ab9a9ba6e72218883a8ff899b82d8a803
Stefanrmc <stefanrcole@gmail.com>
2025-06-18 15:59:19 +0100

    Add main handler file and create clients.

---
+++ report/main.py
@@ -0,0 +1,27 @@
+"""Lambda handler script for reports email script."""
+
+import logging
+from os import environ as ENV
+
+from dotenv import load_dotenv
+
+from extract_reports import get_db_connection, get_station_name_from_crs
+from transform_summary import get_station_summary
+from report import get_email_message_as_string
+from load import load_new_report, get_s3_client
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel("DEBUG")
+
+ses_client = boto3.client("ses", aws_access_key_id=ENV["AWS_ACCESS_KEY_ID"],
+                          aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"])
+sns_client = boto3.client("sns", aws_access_key_id=ENV["AWS_ACCESS_KEY_ID"],
+                          aws_secret_access_key=ENV["AWS_SECRET_ACCESS_KEY"])
+
+
+def lambda_handler(event, context) -> dict:
+    """AWS Lambda handler that runs the ETL pipeline for summary reports."""
+    load_dotenv()
+    try:
+        logger.info("Lambda triggered, checking for existing PDF report in S3.")
