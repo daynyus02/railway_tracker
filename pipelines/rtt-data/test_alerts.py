e9f48b79b9f7876c47d31d6ea3dd64d41c619e31
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-18 15:47:30 +0100

    Add test for alerts.py

--- pipelines/rtt-data/test_alerts.py
+++ pipelines/rtt-data/test_alerts.py
@@ -1,9 +1,12 @@
 """Test for alert functions that send alerts/notifications to a user."""
 # pylint: skip-file
-
+from alerts import send_notification
+import logging
+import pandas as pd
 from pandas import DataFrame
 from datetime import time
-from alerts import get_delayed_trains
+from unittest.mock import patch, MagicMock
+from alerts import get_delayed_trains, filter_by_route, send_notification
 
 
 def test_get_delayed_trains():
@@ -19,3 +22,26 @@ def test_get_delayed_trains():
     assert len(delayed_trains) == 1
     assert delayed_trains.iloc[0]["scheduled_dep_time"] == time(14, 0)
     assert delayed_trains.iloc[0]["actual_dep_time"] == time(14, 5)
+
+
+def test_filter_by_route():
+    test_data = {
+        "origin_name":
+            ["London Paddington", "London Paddington",
+                "Edinburgh", "London Paddington"],
+        "destination_name": [
+            "Bristol Temple Meads", "Oxford", "Bristol Temple Meads", "Bristol Temple Meads"
+        ],
+    }
+
+    test_df = DataFrame(test_data)
+
+    result = filter_by_route(
+        test_df, "London Paddington", "Bristol Temple Meads")
+    assert len(result) == 2
+
+    result = filter_by_route(test_df, "Leeds", "Bristol Temple Meads")
+    assert result.empty
+
+    result = filter_by_route(test_df, "Edinburgh", "Bristol Temple Meads")
+    assert len(result) == 1
