dea658a82bb0330049f0a802830b4b639e61e472
daynyus02 <daynyus.halinauskas02@gmail.com>
2025-06-19 10:24:20 +0100

    Update alerts.py

--- pipelines/rtt-data/alerts.py
+++ pipelines/rtt-data/alerts.py
@@ -1,8 +1,6 @@
 """Modules/Functions required to push information to station topics."""
 import logging
-from datetime import datetime, date
 from pandas import DataFrame
-from datetime import timedelta
 import boto3
 
 
@@ -15,34 +13,22 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def get_delayed_trains(api_train_data: DataFrame) -> DataFrame:
-    """Returns a dataframe that contains only delayed trains."""
-    delayed_trains_mask = (
-        api_train_data["actual_dep_time"] >
-        api_train_data["scheduled_dep_time"]
-    )
-    delayed_trains = api_train_data[delayed_trains_mask]
-    logging.info(f"Found {len(delayed_trains)} delayed train(s).")
-    return delayed_trains
-
-
-def filter_by_route(api_train_data: DataFrame, origin: str, destination: str) -> DataFrame:
+def filter_by_route(delayed_train_data: DataFrame, origin: str, destination: str) -> DataFrame:
     """Filters delayed trains for a specific route."""
-    filtered_route = api_train_data[
-        (api_train_data["origin_name"] == origin) &
-        (api_train_data["destination_name"] == destination)
+    filtered_route = delayed_train_data[
+        (delayed_train_data["origin_name"] == origin) &
+        (delayed_train_data["destination_name"] == destination)
     ]
     logging.info(
-        f"Filtered {len(filtered_route)} delayed train(s) for route: {origin} → {destination}.")
+        "Filtered %s delayed train(s) for route: %s → %s.",
+        len(filtered_route), origin, destination
+    )
     return filtered_route
 
 
-def send_notification(api_train_data: DataFrame) -> None:
+def send_notification(delayed_train_data: DataFrame) -> None:
     """Sends notification to specific route topics."""
     sns_client = boto3.client("sns")
-
-    logging.info("Filtering delayed trains...")
-    delayed_trains = get_delayed_trains(api_train_data)
 
     routes = [
         ("London Paddington", "Bristol Temple Meads")
@@ -54,35 +40,30 @@ def send_notification(api_train_data: DataFrame) -> None:
     }
 
     for origin, destination in routes:
-        logging.info(f"Filtering for {origin} -> {destination}")
+        logging.info("Filtering for %s -> %s", origin, destination)
         delayed_train_for_current_route = filter_by_route(
-            delayed_trains, origin, destination)
+            delayed_train_data, origin, destination)
 
         if delayed_train_for_current_route.empty:
             logging.info(
-                f"No delays found for {origin} -> {destination} route")
+                "No delays found for %s -> %s", origin, destination)
         else:
             message = [
                 f"🚨Delayed Departures: {origin} -> {destination}🚨\n"
             ]
 
             for _, row in delayed_train_for_current_route.iterrows():
-                scheduled = datetime.combine(
-                    date.today(), row["scheduled_dep_time"])
-                actual = datetime.combine(date.today(), row["actual_dep_time"])
-                delay_minutes = int((actual - scheduled).total_seconds() // 60)
-
                 station = (
-                    f"{row['station_name']}\n"
-                    f"Scheduled: {row['scheduled_dep_time']}\n"
-                    f"Actual: {row['actual_dep_time']}\n"
-                    f"Delay: {delay_minutes} min\n"
+                    f"Departing From: {row['station_name']}\n"
+                    f"Scheduled: {row['scheduled_dep_time_api']}\n"
+                    f"Actual: {row['actual_dep_time_api']}\n"
+                    f"Delay: {row["delay_new"]} min\n"
                     f"Platform: {row.get('platform', 'N/A')}"
                 )
 
                 message.append(station)
 
-            full_message = "\n".join(message)
+            full_message = "\n\n".join(message)
 
             topic_name = f"c17-trains-delays-{routes_to_crs[origin]}-{routes_to_crs[destination]}"
             response = sns_client.create_topic(Name=topic_name)
@@ -94,4 +75,4 @@ def send_notification(api_train_data: DataFrame) -> None:
                 Subject=f"Delays: {origin} → {destination}"
             )
 
-            logging.info(f"Published delay alert to SNS topic {topic_arn}")
+            logging.info("Published delay alert to SNS topic %s", topic_arn)
